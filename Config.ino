7783903f64c525b56fd77c05438a1610dee27f4f
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2021-08-21 17:34:46 -0500

    create struct process and finish program v0.0.1

--- Config.ino
+++ Config.ino
@@ -1,13 +1,17 @@
 // Configuracion
 
-AnalogPin  		Selector_on    	(A4);
-AnalogPin  		Selector_off	(A5);
-DigitalPin 		Selector_timer	(6);
-DigitalPin 		Selector_sensor	(7);
-DigitalPin 		Sensor_PIR		(4);
-Led 			Led_test		(13);
-LedPWM 			Led_state		(11);
-DigitalActuator Relay  			(2);
+AnalogPin  		Selector_on    		(A4);
+AnalogPin  		Selector_off		(A5);
+DigitalPin 		Selector_timer		(6);
+DigitalPin 		Selector_sensor		(7);
+DigitalPin 		Sensor_PIR			(4);
+Led 			Led_test			(13);
+LedPWM 			Led_state_PWM		(11);
+Led 			Led_state_Digital 	(11);
+DigitalActuator Relay  				(2);
 
 SerialConsole 	Msg;
 AsyncDelay 		Timer;
+
+unsigned long MaxTime = 30000;
+unsigned long MinTime = 60000;
--- Main.ino
+++ Main.ino
@@ -6,21 +6,30 @@ void setup() {
 
   Sensor_PIR.In();
   Led_test.Out();
-  Led_state.Out();
+  Led_state_PWM.Out();
+  Led_state_Digital.Out();
   Relay.Out();
   Msg.init();
+
+  Led_state_Digital.useTimeBlink(1000);
+}
+
+void TimerLoop(unsigned long MinValue,unsigned long MaxValue,unsigned long MinTime,unsigned long MaxTime){
+  Timer.start(map(Selector_on.Read(),0,600,MinTime,MaxTime),AsyncDelay::MILLIS);
+  while(!Timer.isExpired()){
+    Led_state_PWM.blink(1000,100);
+    Relay.On();
+  }
+  Timer.start(map(Selector_off.Read(),0,600,MinTime,MaxTime),AsyncDelay::MILLIS);
+  while(!Timer.isExpired()){
+    Led_state_Digital.Off();
+    Relay.Off();
+  }
 }
 
 void loop() {
-  while(Selector_timer.If(true)&&Selector_sensor.If(false)){
-    // Led_test.Set(0);
-  }
-  while(Selector_timer.If(false)&&Selector_sensor.If(true)){
-  }
-  while(Selector_timer.If(false)&&Selector_sensor.If(false)){
-      Led_state.blink(1000,10);
-      Led_state.blink(1000,100);
-      Led_state.blink(1000,200);
-  }
-  Led_state.Set(0);
+  while(Selector_timer.If(true)&&Selector_sensor.If(false))TimerLoop(0,600,MinTime,MaxTime);
+  while(Selector_timer.If(false)&&Selector_sensor.If(true))if(Sensor_PIR.Read()==true)TimerLoop(0,600,MinTime,MaxTime);
+  while(Selector_timer.If(false)&&Selector_sensor.If(false))if(Sensor_PIR.If(true))Led_state_Digital.On();else Led_state_Digital.Off();
+  Led_state_Digital.Off();
 }
