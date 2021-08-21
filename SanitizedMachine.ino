01206846e91f682dfba479201232e87761944c63
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2021-08-20 19:47:38 -0500

    add basic class for using pins

---
+++ SanitizedMachine.ino
@@ -0,0 +1,72 @@
+class Pin{
+  private:
+    int _pin = 0;
+  public:
+    Pin(int pin){
+      this->_pin = pin;
+    }
+    int getPin(){
+      return this->_pin;
+    }
+    void Out(){
+      pinMode(this->_pin,OUTPUT);
+    }
+    void In(){
+      pinMode(this->_pin,INPUT);
+    }
+};
+
+class DigitalPin: public Pin {
+  public:
+    DigitalPin(int pin):Pin(pin){};
+    bool Read(){
+      return digitalRead(this->getPin());
+    }
+    void Write(bool state){
+      digitalWrite(this->getPin(),state);
+    }
+    bool If(bool value){
+      return this->Read()==value;
+    }
+};
+
+class AnalogPin: public Pin {
+  public:
+    AnalogPin(int pin):Pin(pin){};
+    int Read(){
+      return analogRead(this->getPin());
+    }
+    void Write(int state){
+      analogWrite(this->getPin(),state);
+    }
+    bool If(int value){
+      return this->Read()==value;
+    }
+};
+
+class DigitalActuator: public DigitalPin{
+  public:
+    DigitalActuator(int pin):DigitalPin(pin){}
+    void On(){
+      this->Write(true);
+    }
+    void Off(){
+      this->Write(false);
+    }
+    void Switch(){
+      if(this->If(true))this->Off();
+      else if(this->If(false))this->On();
+    }
+};
+
+class AnalogActuator: public AnalogPin{
+  public:
+    AnalogActuator(int pin):AnalogPin(pin){}
+    void Set(int number){
+      this->Write(number);
+    }
+    void Switch(int rangeOn, int rangeOff){
+      if(this->If(rangeOn))this->Set(rangeOff);
+      else if(this->If(rangeOff))this->Set(rangeOn);
+    }
+};
